225520117890e0733067ef5dd7f5c22f0016fb1b
bibhashhalder <halderbibhash80@gmail.com>
2023-11-29 12:42:18 +0600

    schema branch added

--- src/app/config/modules/users/users.schema.ts
+++ src/app/config/modules/users/users.schema.ts
@@ -1,28 +1,31 @@
 import { Schema, model, connect } from 'mongoose';
-import { Users } from './users.interface';
+import { Address, FullName, Order, Users } from './users.interface';
+const fullNameSchema =new Schema<FullName>({
+    firstName:{type:String, required:true},
+    lastName:{type:String, required:true}
+})
+const addressSchema =new Schema<Address>({
+    street:{type:String,required:true},
+    city:{type:String, required:true},
+    country:{type:String,required:true}
+})
 
+const ordersSchema = new Schema<Order>({
+    productName:{type:String,require:true},
+    price:{type:Number, require:true},
+    quantity:{type:String,require:true},
+})
 const usersSchema = new Schema<Users>({
     userId:{type:Number, required:true},
     userName:{type:String, required:true},
     password:{type:String, required:true},
-    fullName:{
-        firstName:{type:String, required:true},
-        lastName:{type:String, required:true}
-    },
+    fullName:fullNameSchema,
     age:{type:Number, required:true},
     email:{type:String, required:true},
     isActive:{type:Boolean,required:true},
     hobbies:{type:[String], required:true},
-    address:{
-        street:{type:String,required:true},
-        city:{type:String, required:true},
-        country:{type:String,required:true}
-    },
-    orders:{
-        productName:{type:String,require:true},
-        price:{type:String, require:true},
-        quantity:{type:String,require:true}
-    }
+    address:addressSchema,
+    orders:ordersSchema
 })
 
 const users = model<Users>('Users', usersSchema )
